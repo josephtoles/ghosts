f5778e36d9a62288a0465fa3ed2ce16445955adb
Joseph Toles <jbtoles@gmail.com>
2015-04-13 16:06:38 -0700

    Moved shell script

--- curl.sh
+++
@@ -1,4 +0,0 @@
-CLIENT_ID=KCX65QW3W0
-CLIENT_SECRET=f8ZUYL4v4QuaBhSL44MTnyOdxSCLxg
-curl -XPOST -d 'client_id='$CLIENT_ID'&client_secret='$CLIENT_SECRET'&grant_type=authorization_code&redirect_uri=REDIRECT_URI&code=CODE' http://localhost:1337/oauth/token
-
