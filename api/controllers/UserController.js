f79912f894723934a29ac94ee1d99d51c3ff0a74
Joseph Toles <jbtoles@gmail.com>
2015-04-14 15:27:11 -0700

    Disabled controller functions

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -12,30 +12,36 @@ module.exports = {
   /**
    * `TestController.create()`
    */
+  /*
   create: function (req, res) {
     return res.json({
       todo: 'create() is not implemented yet!'
     });
   },
+  */
 
 
   /**
    * `TestController.destroy()`
    */
+  /*
   destroy: function (req, res) {
     return res.json({
       todo: 'destroy() is not implemented yet!'
     });
   },
+  */
 
 
   /**
    * `TestController.update()`
    */
+  /*
   update: function (req, res) {
     return res.json({
       todo: 'update() is not implemented yet!'
     });
   }
+  */
 };
 
